f10246e97b7eac51b4203009179c17691eadb42c
savaramsiddhartha05@gmail.com <savaramsiddhartha05@gmail.com>
2026-07-04 03:17:33 +0530

    feat: added scheduled background cleanup for expired cache entries

--- src/main/java/implementations/InMemoryCache.java
+++ src/main/java/implementations/InMemoryCache.java
@@ -4,30 +4,45 @@ import cache.Cache;
 import metrics.CacheMetrics;
 import model.CacheEntry;
 
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class InMemoryCache<K,V> implements Cache<K,V> {
-    private final ConcurrentHashMap<K, CacheEntry<V>> cache;
+    private final ConcurrentHashMap<K, CacheEntry<V>> Cache;
     private final CacheMetrics metrics;
+    private final ScheduledExecutorService scheduler;
+    private final long cleanupIntervalMillis;
+    public InMemoryCache() {
+        this(20000);
+    }
 
-    public InMemoryCache(){
-        this.cache=new ConcurrentHashMap<>();
-        this.metrics=new CacheMetrics();
+    public InMemoryCache(long cleanupIntervalMillis){
+        Cache =new ConcurrentHashMap<>();
+        metrics=new CacheMetrics();
+        scheduler = Executors.newSingleThreadScheduledExecutor();
+        this.cleanupIntervalMillis=cleanupIntervalMillis;
+        Runnable cleanUpTask=() ->{
+            removeExpiredEntries();
+        };
+        scheduler.scheduleWithFixedDelay(cleanUpTask,cleanupIntervalMillis, cleanupIntervalMillis, TimeUnit.MILLISECONDS);
     }
 
     @Override
     public void put(K key, V value) {
-        cache.put(key, new CacheEntry<>(value));
+        Cache.put(key, new CacheEntry<>(value));
     }
 
     @Override
     public void put(K key, V value, long ttlMillis) {
-        cache.put(key, new CacheEntry<>(value,ttlMillis));
+        Cache.put(key, new CacheEntry<>(value,ttlMillis));
     }
 
     @Override
     public V get(K key) {
-        CacheEntry<V> entry=cache.get(key);
+        CacheEntry<V> entry= Cache.get(key);
         if(entry==null){
             metrics.incrementMisses();
             return null;// returning null is industry standard
@@ -46,7 +61,7 @@ public class InMemoryCache<K,V> implements Cache<K,V> {
     @Override
     public void remove(K key) {
 
-        CacheEntry<V> removedEntry=cache.remove(key);
+        CacheEntry<V> removedEntry= Cache.remove(key);
         if(removedEntry!=null) {
             // future metrics logic
         }
@@ -54,7 +69,7 @@ public class InMemoryCache<K,V> implements Cache<K,V> {
 
     @Override
     public boolean containsKey(K key) {
-        CacheEntry<V> entry=cache.get(key);
+        CacheEntry<V> entry= Cache.get(key);
         if(entry==null){
             return false;
         }
@@ -68,19 +83,46 @@ public class InMemoryCache<K,V> implements Cache<K,V> {
 
     @Override
     public int size() {
-        return cache.size();
+        return Cache.size();
         // this is not accurate. we are removing entries lazily. there are high chances that the expired entries still exists in our cache map.
     }
 
     public void clear() {
-        cache.clear();
+        Cache.clear();
     }
 
     private boolean removeExpiredEntry(K key,CacheEntry<V> value){
-        boolean removed= cache.remove(key,value);
+        boolean removed= Cache.remove(key,value);
         if(removed){
             this.metrics.incrementExpiredEntries();
         }
         return removed;
     }
+
+    public void shutdown(){
+        scheduler.shutdown();
+        try{
+            if(!scheduler.awaitTermination(10,TimeUnit.SECONDS)){
+                scheduler.shutdownNow();
+            }
+        }catch (InterruptedException e){
+            scheduler.shutdownNow();
+            Thread.currentThread().interrupt();
+        }
     }
+
+
+    public void removeExpiredEntries(){
+        try{
+            for(Map.Entry<K,CacheEntry<V>> cache: Cache.entrySet()){
+                CacheEntry<V> cacheEntry= cache.getValue();
+                if(cacheEntry.isExpired()){
+                    removeExpiredEntry(cache.getKey(),cacheEntry);
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            // logging will be implemented in the future
+        }
+    }
+}
